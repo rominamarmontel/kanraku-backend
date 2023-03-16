5e16dea6a0f03cffeb394a9362df045c8273796a
Sophie Dtr <119942370+So-Mina@users.noreply.github.com>
2023-03-16 14:54:00 +0100

    clean code

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,12 +6,15 @@ router.get("/", (req, res, next) => {
 
 router.use("/auth", require("./auth.routes"));
 
-/* ADMIN ROUTES */
-router.use("/dashboard", require("./dashboard.routes"));
-
 router.use("/user", require("./user.routes"));
 router.use("/orders", require("./orders.routes"));
 router.use("/products", require("./products.routes"));
 router.use("/cart", require("./cart.routes"));
+
+// error handling middleware
+router.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 module.exports = router;
